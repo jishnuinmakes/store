358356e936ef8a947112d627ee491dd68609a1a9
jishnuinmakes <jishnukkm4@gmail.com>
2024-03-14 12:47:24 +0530

    first commit

---
+++ schoolapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Details, Courses, Department
+
+# Register your models here.
+admin.site.register(Details)
+admin.site.register(Courses)
+admin.site.register(Department)
